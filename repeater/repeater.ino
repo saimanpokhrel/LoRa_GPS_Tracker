755c7f0283fea29c8a7b517390d45a26dcb7d46c
saimanpokhrel <55702023+saimanpokhrel@users.noreply.github.com>
2024-06-15 01:25:14 +0545

    Update repeater.ino

--- repeater/repeater.ino
+++ repeater/repeater.ino
@@ -1,9 +1,49 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include <LoRa.h>
 
+const int csPin = 10;
+const int resetPin = 9;
+const int dio0Pin = 2;
+
+String receivedData;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("LoRa Receiver and Repeater");
+  LoRa.setPins(csPin, resetPin, dio0Pin);
+
+  if (!LoRa.begin(433E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  Serial.println("LoRa Initializing OK!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    Serial.print("Received packet '");
 
+    while (LoRa.available()) {
+      receivedData += (char)LoRa.read();
+    }
+
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+
+    retransmitData(receivedData);
+    receivedData = "";
+  }
+}
+
+void retransmitData(String data) {
+  Serial.print("Retransmitting packet: ");
+  Serial.println(data);
+
+  LoRa.beginPacket();
+  LoRa.print(data);
+  LoRa.endPacket();
 }
