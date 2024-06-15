3343cae832430b6411a6f618c972c1964fda8993
Gaurav <gaurav.dulal.50@gmail.com>
2024-06-15 16:17:11 +0545

    added the map file and also changed the map creator

--- map_creator.py
+++ map_creator.py
@@ -1,6 +1,6 @@
 import folium
 
 def create_map(lat, lon):
-    map = folium.Map(location=[lat, lon], zoom_start=15)
-    folium.Marker([lat, lon], popup="GPS Location").add_to(map)
-    map.save("static/map.html")
+    map_obj = folium.Map(location=[lat, lon], zoom_start=15)
+    folium.Marker([lat, lon], popup="GPS Location").add_to(map_obj)
+    map_obj.save("static/map.html")
