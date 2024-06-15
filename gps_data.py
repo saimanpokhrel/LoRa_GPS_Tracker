1848c5f32d593bbe0e04ff11b188b757025aae40
Gaurav <gaurav.dulal.50@gmail.com>
2024-06-15 10:54:39 +0545

    updated the gps data collector

--- gps_data.py
+++ gps_data.py
@@ -4,8 +4,3 @@ def parse_gps_data(data):
         return lat, lon
     except ValueError:
         return None, None
-
-# Example usage:
-gps_data = "12.345678,98.765432"
-latitude, longitude = parse_gps_data(gps_data)
-print(f"Latitude: {latitude}, Longitude: {longitude}")
